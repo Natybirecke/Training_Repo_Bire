328002ad5f4b32bd3aee3dffcfaeb3d400af4ef4
Birhanu <105836298+Natybirecke@users.noreply.github.com>
2022-05-23 00:09:32 -0400

    Update Training_Repo_Bire.py
    Added Menu

--- Training_Repo_Bire.py
+++ Training_Repo_Bire.py
@@ -1,3 +1,8 @@
 import streamlit
 streamlit.title('My Parents New Healthy Diner')
-streamlit.title('Breakfast Menu')
+streamlit.header('Breakfast Menu')
+streamlit.text('Eggs')
+streamlit.text('Meat')
+streamlit.text('Injira')
+streamlit.text('Burgers')
+streamlit.text('Green Burritos')
