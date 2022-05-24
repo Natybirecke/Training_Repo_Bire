7137db0a3c69a7759af178b97254cdd52d31ab38
Birhanu <105836298+Natybirecke@users.noreply.github.com>
2022-05-23 23:17:02 -0400

    Update Training_Repo_Bire.py

--- Training_Repo_Bire.py
+++ Training_Repo_Bire.py
@@ -1,6 +1,7 @@
 import streamlit
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Menu')
+streamlit.header('\N{egg} Eggs \N{egg}')
 streamlit.text('Eggs')
 streamlit.text('Meat')
 streamlit.text('Injira')
