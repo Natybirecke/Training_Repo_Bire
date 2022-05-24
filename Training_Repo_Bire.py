c1d6d5aa3fab7c9ea000714f89a85cf888398954
Birhanu <105836298+Natybirecke@users.noreply.github.com>
2022-05-23 23:43:17 -0400

    Update Training_Repo_Bire.py

--- Training_Repo_Bire.py
+++ Training_Repo_Bire.py
@@ -6,6 +6,8 @@ streamlit.text('🌮🌮  Taco 🌮🌮')
 streamlit.text('🍗🍗 Meat 🍗🍗')
 streamlit.text('🍕🍕 Pizza 🍕🍕')
 streamlit.text('🍔 Burgers 🍔🍔')
+streamlit.title('My smoothies')
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.multiselect("pick some fruits:",list(my_fruit_list.index))
 streamlit.dataframe(my_fruit_list)
