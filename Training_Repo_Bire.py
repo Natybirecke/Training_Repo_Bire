17a0262ea9bb517a8d53e99f2dac27c2d9f490c8
Birhanu <105836298+Natybirecke@users.noreply.github.com>
2022-05-23 23:35:36 -0400

    Update Training_Repo_Bire.py

--- Training_Repo_Bire.py
+++ Training_Repo_Bire.py
@@ -6,4 +6,6 @@ streamlit.text('🌮🌮  Taco 🌮🌮')
 streamlit.text('🍗🍗 Meat 🍗🍗')
 streamlit.text('🍕🍕 Pizza 🍕🍕')
 streamlit.text('🍔 Burgers 🍔🍔')
-
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
