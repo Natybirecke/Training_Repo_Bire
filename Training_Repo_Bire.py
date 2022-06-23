bce034fa1d82abea1039c763d7bc9987a532dcae
Birhanu <105836298+Natybirecke@users.noreply.github.com>
2022-06-22 23:22:16 -0400

    Update Training_Repo_Bire.py

--- Training_Repo_Bire.py
+++ Training_Repo_Bire.py
@@ -22,3 +22,9 @@ Fruit_response = requests.get("https://fruityvice.com/api/fruit/" + Fruit_user_i
 Fruit_Normalize = pandas.json_normalize(Fruit_response.json())
 streamlit.dataframe(Fruit_Normalize)
 import snowflake.connector
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
