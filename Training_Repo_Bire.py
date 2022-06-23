a5835a8434b599488db6efd566210538b123063d
Birhanu <105836298+Natybirecke@users.noreply.github.com>
2022-06-23 00:04:12 -0400

    Update Training_Repo_Bire.py

--- Training_Repo_Bire.py
+++ Training_Repo_Bire.py
@@ -13,19 +13,19 @@ streamlit.text('🍕🍕 Pizza 🍕🍕')
 streamlit.text('🍔 Burgers 🍔🍔')
 streamlit.title('My smoothies')
 
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-my_fruit_list = my_fruit_list.set_index('Fruit')
-Fruits_Selecetd = streamlit.multiselect("pick some fruits:",list(my_fruit_list.index),['Avocado','Strawberries'])
-Fruit_to_show = my_fruit_list.loc[Fruits_Selecetd]
-streamlit.dataframe(Fruit_to_show)
+#my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+#my_fruit_list = my_fruit_list.set_index('Fruit')
+#Fruits_Selecetd = streamlit.multiselect("pick some fruits:",list(my_fruit_list.index),['Avocado','Strawberries'])
+#Fruit_to_show = my_fruit_list.loc[Fruits_Selecetd]
+#streamlit.dataframe(Fruit_to_show)
 
 streamlit.title('Fruityvice Fruit advice')
 Fruit_user_input = streamlit.text_input('what fruit do you want to have information about?' , 'kiwi')
 streamlit.write('The user entered',Fruit_user_input)
-Fruit_response = requests.get("https://fruityvice.com/api/fruit/" + Fruit_user_input)
+#Fruit_response = requests.get("https://fruityvice.com/api/fruit/" + Fruit_user_input)
 #streamlit.text(Fruit_response.json())
-Fruit_Normalize = pandas.json_normalize(Fruit_response.json())
-streamlit.dataframe(Fruit_Normalize)
+#Fruit_Normalize = pandas.json_normalize(Fruit_response.json())
+#streamlit.dataframe(Fruit_Normalize)
 
 
 streamlit.stop()
