184108b9ccc61769b26fb7c04feb30046e715e52
Birhanu <105836298+Natybirecke@users.noreply.github.com>
2022-07-03 22:40:43 -0400

    Update Training_Repo_Bire.py

--- Training_Repo_Bire.py
+++ Training_Repo_Bire.py
@@ -21,7 +21,7 @@ streamlit.dataframe(Fruit_to_show)
 def get_fruityvice_data(this_fruit_choice):
     Fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
     Fruityvice_Normalize = pandas.json_normalize(Fruityvice_response.json())
-    retun fruityvice_normalized   
+    return fruityvice_normalized   
 streamlit.header('Fruityvice Fruit advice')
 try:
   Fruit_choice = streamlit.text_input('what fruit do you want to have information about?')
