39f4ea722627cef0742cc65067cd8ef109576b3d
Birhanu <105836298+Natybirecke@users.noreply.github.com>
2022-07-04 23:13:19 -0400

    Update Training_Repo_Bire.py

--- Training_Repo_Bire.py
+++ Training_Repo_Bire.py
@@ -40,7 +40,7 @@ streamlit.header("Fruit list contains:")
 def get_fruit_load_list():
     with my_cnx.cursor() as my_cur:
          my_cur.execute("select * from fruit_load_list")
-         my_data_row = my_cur.fetchall()
+         return my_cur.fetchall()
 if streamlit.button('get fruit load list'):
     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
     my_data_rows = get_fruit_load_list()
